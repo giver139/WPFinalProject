245d9d3ce3d77a7b62ea1077f2aab494e7004449
giver <ryan01234keroro56789@yahoo.com.tw>
2021-12-14 11:11:58 +0800

    basic router

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,15 @@
 import {runMongo} from './mongo';
 import express from 'express';
 import cors from 'cors';
+import {router} from './router/route';
 
 runMongo();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
 
 const port = Number(process.env.PORT ?? 4000);
 app.listen(port,() => {});
