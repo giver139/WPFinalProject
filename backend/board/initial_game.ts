5dfb6ff96ca97dab7c7a491c3e092395958e2fc2
giver <ryan01234keroro56789@yahoo.com.tw>
2021-12-29 04:28:30 +0800

    create interface

---
+++ backend/board/initial_game.ts
@@ -0,0 +1,7 @@
+export function getFirstMove(): number {
+  return 0;
+}
+
+export function getInitialBoard(): number[] {
+  return new Array(32).fill(0);
+}
--- backend/game/start_game.ts
+++ backend/game/start_game.ts
@@ -5,6 +5,7 @@ import {RoomNotFoundError} from '../room/error';
 import {GameCounterNotFoundError, PlayerNumberUnmatchError} from './error';
 import {GameView} from '../views/game';
 import {WebSocketConnection} from '../websocket/web_socket_connection';
+import {getFirstMove, getInitialBoard} from '../board/initial_game';
 
 export async function startGame(roomId: number): Promise<GameView> {
   const gameCounter = await CounterModel.findOne({model: GAME_MODEL});
@@ -20,7 +21,7 @@ export async function startGame(roomId: number): Promise<GameView> {
   if(room.players.length !== 2) {
     throw new PlayerNumberUnmatchError;
   }
-  const newGame = new GameModel({gameId, players: [...room.players], timestamp: Date.now(), firstMove: 0, board: []});
+  const newGame = new GameModel({gameId, players: [...room.players], timestamp: Date.now(), firstMove: getFirstMove(), board: getInitialBoard()});
   await room.remove();
   await gameCounter.save();
   await newGame.save();
