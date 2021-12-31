e7648f83501b0c4720d2580550bcd8466a5e515a
yp155136 <yp155136@gmail.com>
2021-12-31 17:37:44 +0800

    first commit, Hooray

--- backend/board/get_valid_destinations.ts
+++ backend/board/get_valid_destinations.ts
@@ -1,8 +1,11 @@
 import {InvalidSourceSelectionError, NoPossibleDestinationError} from './error';
-import {Color, Move, Position, Board} from '../board/models';
+import {Color, Move, Position, Board, ChessNo} from '../board/models';
 import {Game} from '../models/game';
 
 export function getValidDestinations(game: Readonly<Game>, color: Color, source: Position): Move[] {
   const board = new Board(game.board);
+  if (board.board[source.index].chessNo === ChessNo.EMPTY) {
+    throw new InvalidSourceSelectionError;
+  }
   return [];
 }
